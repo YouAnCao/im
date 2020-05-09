2b05a73ff35c0d1617fd23854bf2e15930516910
Lyon.Cao <lyon.cao@yahoo.com>
2020-05-10 03:05:27 +0800

    change the protocol file

--- commons/src/main/java/com/hd/im/proto/Tester.java
+++ commons/src/main/java/com/hd/im/proto/Tester.java
@@ -1,12 +1,21 @@
 package com.hd.im.proto;
 
+import cn.hutool.core.codec.Base64;
 import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hd.im.utils.AESUtils;
 
 public class Tester {
-	public static void main(String[] args) {
-		HDIMProtocol.Login login = HDIMProtocol.Login.newBuilder().setToken("token")
-				.setEncryptData(ByteString.copyFrom("encrypt".getBytes()))
-				.setEncryptKey(ByteString.copyFrom("key".getBytes())).build();
-		System.out.println(login);
-	}
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        HDIMProtocol.Login login = HDIMProtocol.Login.newBuilder().setToken("token")
+                .setEncryptData(ByteString.copyFrom("encrypt".getBytes()))
+                .setEncryptKey(ByteString.copyFrom("key".getBytes())).build();
+        byte[] encrypt = AESUtils.encrypt(login.toByteArray(), "123456");
+        String encode = Base64.encode(encrypt);
+        System.out.println("加密后内容：" + encode);
+        System.out.println("开始解密");
+        byte[] decrypt = AESUtils.decrypt(Base64.decode(encode), "123456");
+        HDIMProtocol.Login login1 = HDIMProtocol.Login.parseFrom(decrypt);
+        System.out.println(login1);
+    }
 }
--- commons/src/main/java/com/hd/im/utils/AESUtils.java
+++ commons/src/main/java/com/hd/im/utils/AESUtils.java
@@ -21,24 +21,21 @@ public class AESUtils {
      * @param password 加密需要的密码
      * @return 密文
      */
-    public static byte[] encrypt(String content, String password) {
+    public static byte[] encrypt(byte[] content, String password) {
         try {
             KeyGenerator kgen = KeyGenerator.getInstance("AES");// 创建AES的Key生产者
             kgen.init(128, new SecureRandom(password.getBytes()));// 利用用户密码作为随机数初始化出
             //加密没关系，SecureRandom是生成安全随机数序列，password.getBytes()是种子，只要种子相同，序列就一样，所以解密只要有password就行
-            SecretKey     secretKey    = kgen.generateKey();// 根据用户密码，生成一个密钥
-            byte[]        enCodeFormat = secretKey.getEncoded();// 返回基本编码格式的密钥，如果此密钥不支持编码，则返回
-            SecretKeySpec key          = new SecretKeySpec(enCodeFormat, "AES");// 转换为AES专用密钥
-            Cipher        cipher       = Cipher.getInstance("AES");// 创建密码器
-            byte[]        byteContent  = content.getBytes("utf-8");
+            SecretKey secretKey = kgen.generateKey();// 根据用户密码，生成一个密钥
+            byte[] enCodeFormat = secretKey.getEncoded();// 返回基本编码格式的密钥，如果此密钥不支持编码，则返回
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");// 转换为AES专用密钥
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
             cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化为加密模式的密码器
-            byte[] result = cipher.doFinal(byteContent);// 加密
+            byte[] result = cipher.doFinal(content);// 加密
             return result;
         } catch (NoSuchPaddingException e) {
             e.printStackTrace();
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (InvalidKeyException e) {
             e.printStackTrace();
@@ -50,7 +47,7 @@ public class AESUtils {
         return null;
     }
 
-    private Cipher getCipher(String password){
+    private Cipher getCipher(String password) {
         return null;
     }
 
@@ -65,10 +62,10 @@ public class AESUtils {
         try {
             KeyGenerator kgen = KeyGenerator.getInstance("AES");// 创建AES的Key生产者
             kgen.init(128, new SecureRandom(password.getBytes()));
-            SecretKey     secretKey    = kgen.generateKey();// 根据用户密码，生成一个密钥
-            byte[]        enCodeFormat = secretKey.getEncoded();// 返回基本编码格式的密钥
-            SecretKeySpec key          = new SecretKeySpec(enCodeFormat, "AES");// 转换为AES专用密钥
-            Cipher        cipher       = Cipher.getInstance("AES");// 创建密码器
+            SecretKey secretKey = kgen.generateKey();// 根据用户密码，生成一个密钥
+            byte[] enCodeFormat = secretKey.getEncoded();// 返回基本编码格式的密钥
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");// 转换为AES专用密钥
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
             cipher.init(Cipher.DECRYPT_MODE, key);// 初始化为解密模式的密码器
             byte[] result = cipher.doFinal(content);
             return result; // 明文
@@ -87,14 +84,14 @@ public class AESUtils {
     }
 
     public static void main(String[] args) throws Exception {
-        String content  = "hello world!";
+        String content = "hello world!";
         String password = "123321";
         System.out.println("需要加密的内容：" + content);
-        byte[] encrypt     = encrypt(content, password);
+        byte[] encrypt = encrypt(content.getBytes(), password);
         String encryptData = new String(Base64.getEncoder().encode(encrypt));
-        System.out.println("加密完成后:"+encryptData);
+        System.out.println("加密完成后:" + encryptData);
         encrypt = Base64.getDecoder().decode(encryptData);
         byte[] decrypt = decrypt(encrypt, password);
-        System.out.println("解密完成:"+new String(decrypt));
+        System.out.println("解密完成:" + new String(decrypt));
     }
 }
